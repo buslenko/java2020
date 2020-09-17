d984f27c9dffee5d83e2856b375450882c24d4a5
Anastasiya Buslenko <anastasiyabuslenko@gmail.com>
2020-09-18 00:03:15 +0300

    lab3

---
+++ src/com/company/lab3/Hand.java
@@ -0,0 +1,17 @@
+package com.company.lab3;
+
+public class Hand {
+    private int fingers;
+
+    public Hand(int fingers){
+        this.fingers=fingers;
+    }
+
+    public int getFingers() {
+        return fingers;
+    }
+
+    public void setFingers(int fingers) {
+        this.fingers = fingers;
+    }
+}
