03f97b5d9107b90977056622460bd87c1deb1307
Anastasiya Buslenko <anastasiyabuslenko@gmail.com>
2020-10-13 23:09:34 +0300

    lab12

---
+++ src/com/company/lab12/Main.java
@@ -0,0 +1,43 @@
+package com.company.lab12;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Main extends JFrame {
+
+    public static final int width = 800;
+    public static final int height = 600;
+    private static final double G = 6.67;
+    public Main(){
+        super("Planets");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        JButton btn = new JButton("Click");
+        JComboBox planetList = new JComboBox(Planets.values());
+        JLabel label1 = new JLabel("Гравитации Земли = 9.8");
+        JPanel pane = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Planets planets = (Planets)planetList.getSelectedItem();
+
+                double grav = planets.gravity() / 1000000000000.00;
+                label1.setText("Гравитация " + planets.getName() + " = " + grav);
+            }
+        });
+        pane.add(label1);
+        pane.add(btn);
+        pane.add(planetList);
+        setContentPane(pane);
+        setSize(width, height);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
+
