982a06613bd9419f69f38c033fff2db4759f77dd
Anastasiya Buslenko <anastasiyabuslenko@gmail.com>
2020-10-20 23:23:39 +0300

    lab13

---
+++ src/com/company/lab14/Comparable.java
@@ -0,0 +1,5 @@
+package com.company.lab14;
+
+public interface Comparable {
+    public int compare(Object o,int n);
+}
---
+++ src/com/company/lab14/Comparator.java
@@ -0,0 +1,5 @@
+package com.company.lab14;
+
+public interface Comparator {
+    int compareTo(Student one, Student two, int i);
+}
