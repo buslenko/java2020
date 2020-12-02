f8f3f3620a2e2466c433a30d83215ad6e2c72f59
busenko <nastya07112001>
2020-12-02 13:18:23 +0300

    lab24

---
+++ src/com/company/lab24/ChairAbstractFactory.java
@@ -0,0 +1,8 @@
+package com.company.lab24;
+
+public interface ChairAbstractFactory {
+    public FunctionChair createFunctionChair();
+    public MagicChair createMagicChair(String material);
+    public VictorianChair createVictorianChair(String owner);
+
+}
