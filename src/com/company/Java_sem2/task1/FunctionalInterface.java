34ae99cd7bfaa9f5b66c0c056ecf793505ddb1fd
Nastya <anastasiyabuslenko@gmail.com>
2021-02-18 16:29:50 +0300

    task1

---
+++ src/com/company/Java_sem2/task1/FunctionalInterface.java
@@ -0,0 +1,5 @@
+package com.company.Java_sem2.task1;
+
+public interface FunctionalInterface<T> {
+    void getResult(int[] value);
+}
---
+++ src/com/company/Java_sem2/task1/Main.java
@@ -0,0 +1,19 @@
+package com.company.Java_sem2.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        int mass [] = new int []{1,2,3,4,5};
+
+
+        FunctionalInterface<Integer> addition = x->a(mass);
+        addition.getResult(mass);
+    }
+
+    public static void a (int mass []){
+        for (int i=4;i>=0;i--){
+            System.out.println(mass[i]);
+        }
+
+    }
+
+}
